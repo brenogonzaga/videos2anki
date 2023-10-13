1bbb1211dc30b50bc8a9eae8948b73d08e91376d
Breno Gonzaga <contato@brenogonzaga.com>
2023-10-12 20:22:27 -0400

    feat: add cut module with video and audio fns using ffmpeg command

--- src/ffmpeg.rs
+++ src/ffmpeg.rs
@@ -29,3 +29,48 @@ pub mod install {
         }
     }
 }
+
+pub mod cut {
+    use std::process::Command;
+
+    pub fn video(start_time: String, end_time: String, input_file: String, output_file: String) {
+        let ffmpeg_video = Command::new("ffmpeg")
+            .arg("-i")
+            .arg(input_file)
+            .arg("-ss")
+            .arg(start_time)
+            .arg("-to")
+            .arg(end_time)
+            .arg("-c:v")
+            .arg("libx264")
+            .arg(output_file)
+            .output()
+            .expect("failed to execute process");
+        if !ffmpeg_video.status.success() {
+            let error_message = String::from_utf8_lossy(&ffmpeg_video.stderr);
+            println!("Error message: {}", error_message);
+            return;
+        }
+    }
+
+    pub fn audio(start_time: String, end_time: String, input_file: String, output_file: String) {
+        let ffmpeg_audio = Command::new("ffmpeg")
+            .arg("-i")
+            .arg(input_file)
+            .arg("-ss")
+            .arg(start_time)
+            .arg("-to")
+            .arg(end_time)
+            .arg("-acodec")
+            .arg("libmp3lame")
+            .arg(output_file)
+            .output()
+            .expect("failed to execute process");
+
+        if !ffmpeg_audio.status.success() {
+            let error_message = String::from_utf8_lossy(&ffmpeg_audio.stderr);
+            println!("Error message: {}", error_message);
+            return;
+        }
+    }
+}
