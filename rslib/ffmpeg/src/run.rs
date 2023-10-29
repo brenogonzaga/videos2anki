7024f69f7cc7a62ee425cda58253cdf16c581004
Breno Gonzaga <contato@brenogonzaga.com>
2023-10-29 18:12:54 -0400

    fix: should work on linux and macos

--- rslib/ffmpeg/src/run.rs
+++ rslib/ffmpeg/src/run.rs
@@ -5,25 +5,26 @@ use std::{
 
 pub fn run_ffmpeg_command(commands: String) -> Result<(), Error> {
     #[cfg(target_os = "windows")]
-    use std::os::windows::process::CommandExt;
-    let mut ffmpeg_process = Command::new("ffmpeg")
-        .args(commands.split(' '))
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .creation_flags(0x08000000) // CREATE_NO_WINDOW
-        .spawn()?;
+    {
+        use std::os::windows::process::CommandExt;
+        let mut ffmpeg_process = Command::new("ffmpeg")
+            .args(commands.split(' '))
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .creation_flags(0x08000000) // CREATE_NO_WINDOW
+            .spawn()?;
+        ffmpeg_process.wait()?;
+    }
 
     #[cfg(not(target_os = "windows"))]
-    let mut ffmpeg_process = Command::new("ffmpeg")
-        .args(commands.split(' '))
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .before_exec(|| {
-            // This code will only run on non-Windows platforms
-            Ok(())
-        })
-        .spawn()?;
+    {
+        let mut ffmpeg_process = Command::new("ffmpeg")
+            .args(commands.split(' '))
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .spawn()?;
+        ffmpeg_process.wait()?;
+    }
 
-    ffmpeg_process.wait()?;
     Ok(())
 }
