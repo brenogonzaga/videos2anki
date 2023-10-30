f9d08bf2e49601968b5ea50df7600bbb7f1cf513
Breno Gonzaga <contato@brenogonzaga.com>
2023-10-29 20:06:39 -0400

    refactor: change bridge binding to use classes

--- rsbridge/lib.rs
+++ rsbridge/lib.rs
@@ -1,66 +1,119 @@
 use std::{fs::File, io::BufReader};
 
 use ffmpeg::cut::{Audio, Video};
-use pyo3::prelude::*;
+use pyo3::{prelude::*, IntoPy};
 
 #[pymodule]
 fn bridge(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(check_ffmpeg, m)?)?;
-    m.add_function(wrap_pyfunction!(generate_audio, m)?)?;
-    m.add_function(wrap_pyfunction!(generate_video, m)?)?;
-    m.add_function(wrap_pyfunction!(generate_csv, m)?)?;
+    m.add_class::<ProcessAudio>()?;
+    m.add_class::<ProcessVideo>()?;
+    m.add_class::<ProcessCsv>()?;
     Ok(())
 }
 
-#[pyfunction]
-fn check_ffmpeg() -> PyResult<()> {
-    ffmpeg::install::check();
-    Ok(())
-}
-
-#[pyfunction]
-fn generate_audio(
-    py: Python,
+#[pyclass]
+struct ProcessAudio {
     input_video: String,
     input_srt: String,
     output_path: String,
     title: String,
-    progress: PyObject,
-) -> PyResult<()> {
-    let (_, times) = generate_times(input_srt);
-    let callback: Box<dyn Fn(u64, u64)> = Box::new(move |done, missing| {
-        let done_obj = done.into_py(py);
-        let missing_obj = missing.into_py(py);
-        let _result = progress.call(py, (done_obj, missing_obj), None);
-    });
-    Audio::new(times, input_video, output_path, title).run(&callback)?;
-    Ok(())
 }
 
-#[pyfunction]
-fn generate_video(
-    py: Python,
+#[pymethods]
+impl ProcessAudio {
+    #[new]
+    fn new(input_video: String, input_srt: String, output_path: String, title: String) -> Self {
+        ProcessAudio {
+            input_video,
+            input_srt,
+            output_path,
+            title,
+        }
+    }
+
+    fn run(&self, progress: PyObject) -> PyResult<()> {
+        let (_, times) = generate_times(self.input_srt.clone());
+        let callback: Box<dyn Fn(u64, u64)> = Box::new(move |done, missing| {
+            Python::with_gil(|py| {
+                let done_obj = done.into_py(py);
+                let missing_obj = missing.into_py(py);
+                let _result = progress.call(py, (done_obj, missing_obj), None);
+            });
+        });
+        Audio::new(
+            times,
+            self.input_video.clone(),
+            self.output_path.clone(),
+            self.title.clone(),
+        )
+        .run(&callback)?;
+        Ok(())
+    }
+}
+
+#[pyclass]
+struct ProcessVideo {
     input_video: String,
     input_srt: String,
     output_path: String,
     title: String,
-    progress: PyObject,
-) -> PyResult<()> {
-    let (_, times) = generate_times(input_srt);
-    let callback: Box<dyn Fn(u64, u64)> = Box::new(move |done, missing| {
-        let done_obj = done.into_py(py);
-        let missing_obj = missing.into_py(py);
-        let _result = progress.call(py, (done_obj, missing_obj), None);
-    });
-    Video::new(times, input_video, output_path, title).run(&callback)?;
-    Ok(())
 }
 
-#[pyfunction]
-fn generate_csv(input_srt: String, output_path: String, title: String) -> PyResult<()> {
-    let (sentences, times) = generate_times(input_srt);
-    file::csv::write(&title, &times, &sentences, &output_path);
-    Ok(())
+#[pymethods]
+impl ProcessVideo {
+    #[new]
+    fn new(input_video: String, input_srt: String, output_path: String, title: String) -> Self {
+        ProcessVideo {
+            input_video,
+            input_srt,
+            output_path,
+            title,
+        }
+    }
+
+    fn run(&self, progress: PyObject) -> PyResult<()> {
+        let (_, times) = generate_times(self.input_srt.clone());
+        let callback: Box<dyn Fn(u64, u64)> = Box::new(move |done, missing| {
+            Python::with_gil(|py| {
+                let done_obj = done.into_py(py);
+                let missing_obj = missing.into_py(py);
+                let _result = progress.call(py, (done_obj, missing_obj), None);
+            });
+        });
+        Video::new(
+            times,
+            self.input_video.clone(),
+            self.output_path.clone(),
+            self.title.clone(),
+        )
+        .run(&callback)?;
+        Ok(())
+    }
+}
+
+#[pyclass]
+struct ProcessCsv {
+    input_srt: String,
+    output_path: String,
+    title: String,
+}
+
+#[pymethods]
+impl ProcessCsv {
+    #[new]
+    fn new(input_srt: String, output_path: String, title: String) -> Self {
+        ProcessCsv {
+            input_srt,
+            output_path,
+            title,
+        }
+    }
+
+    fn run(&self) -> PyResult<()> {
+        let (sentences, times) = generate_times(self.input_srt.clone());
+        file::csv::write(&self.title, &times, &sentences, &self.output_path);
+        Ok(())
+    }
 }
 
 fn generate_times(input_srt: String) -> (Vec<String>, Vec<(String, String)>) {
