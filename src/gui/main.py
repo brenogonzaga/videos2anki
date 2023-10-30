ba63e39c1efe961982defdd503ace7c27abe2186
Breno Gonzaga <contato@brenogonzaga.com>
2023-10-30 00:23:05 -0400

    feat: add button to select srt

--- src/gui/main.py
+++ src/gui/main.py
@@ -8,36 +8,50 @@ class Video2Anki(QtWidgets.QWidget):
         self.setWindowTitle("Video to Anki App")
         self.setGeometry(100, 100, 400, 200)
 
-        self.path_label = QtWidgets.QLabel("Video Path:")
-        self.path_edit = QtWidgets.QLineEdit()
-        self.browse_button = QtWidgets.QPushButton("Browse")
-        self.output_name_label = QtWidgets.QLabel("Output Name:")
-        self.output_name_edit = QtWidgets.QLineEdit()
-        self.output_path_label = QtWidgets.QLabel("Output Path:")
-        self.output_path_edit = QtWidgets.QLineEdit()
+        layout = QtWidgets.QVBoxLayout(self)
+        home_layout = QtWidgets.QFormLayout()
+        layout.addLayout(home_layout)
+
+        self.path_video_label = QtWidgets.QLabel("Video Path:")
+        self.path_video_edit = QtWidgets.QLineEdit()
+        self.browse_video_btn = QtWidgets.QPushButton("Browse")
+        home_layout.addRow(self.path_video_label, self.path_video_edit)
+        home_layout.addRow(self.browse_video_btn)
+
+        self.path_srt_label = QtWidgets.QLabel("SRT Path:")
+        self.path_srt_edit = QtWidgets.QLineEdit()
+        self.browse_srt_btn = QtWidgets.QPushButton("Browse")
+        home_layout.addRow(self.path_srt_label, self.path_srt_edit)
+        home_layout.addRow(self.browse_srt_btn)
+
+
+        self.output_name_label = QtWidgets.QLabel("Deck name:")
+        self.output_name_edit = QtWidgets.QLineEdit("output")
+        home_layout.addRow(self.output_name_label, self.output_name_edit)
+
+        self.output_path_label = QtWidgets.QLabel("Output folder:")
+        self.output_path_edit = QtWidgets.QLineEdit("output")
+        home_layout.addRow(self.output_path_label, self.output_path_edit)
+
         self.run_button = QtWidgets.QPushButton("Run")
         self.progress_label = QtWidgets.QLabel()
 
-        self.browse_button.clicked.connect(self.browse_path)
+        self.browse_video_btn.clicked.connect(self.browse_video)
+        self.browse_srt_btn.clicked.connect(self.browse_srt)
         self.run_button.clicked.connect(self.run_video_to_anki)
 
-        layout = QtWidgets.QVBoxLayout(self)
-        form_layout = QtWidgets.QFormLayout()
-        form_layout.addRow(self.path_label, self.path_edit)
-        form_layout.addRow(self.browse_button)
-        form_layout.addRow(self.output_name_label, self.output_name_edit)
-        form_layout.addRow(self.output_path_label, self.output_path_edit)
-        layout.addLayout(form_layout)
+
         layout.addWidget(self.run_button)
         layout.addWidget(self.progress_label)
 
-    def output_name(self):
-        return self.output_name_edit.text()
-
-    def browse_path(self):
+    def browse_video(self):
         path, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Choose Video File", "", "Video Files (*.mp4 *.avi *.mkv)")
         if path:
-            self.path_edit.setText(path)
+            self.path_video_edit.setText(path)
+    def browse_srt(self):
+        path, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Choose SRT File", "", "SRT Files (*.srt)")
+        if path:
+            self.path_srt_edit.setText(path)
 
     def update_progress(self, done, missing, type):
         self.progress_label.setText(f"{type} - Done: {done}, Missing: {missing}")
@@ -45,15 +59,16 @@ class Video2Anki(QtWidgets.QWidget):
         
 
     def run_video_to_anki(self):
-        video_path = self.path_edit.text()
+        video_path = self.path_video_edit.text()
+        srt_path = self.path_srt_edit.text()
         output_name = self.output_name_edit.text()
         output_path = self.output_path_edit.text()
+
         if not output_path:
             output_path = "output"
         if not output_name:
             output_name = "output"
         if video_path:
-            srt_path = video_path.replace(".mkv", ".srt")
             process_audio = ProcessAudio(video_path, srt_path, output_path, output_name)
             process_video = ProcessVideo(video_path, srt_path, output_path, output_name)
             process_csv = ProcessCsv(srt_path, output_path, output_name)
