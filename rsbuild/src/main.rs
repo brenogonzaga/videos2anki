c4c5e2d9033b96009e54949a122aed72f1fb09b0
Breno Gonzaga <contato@brenogonzaga.com>
2023-11-02 15:09:32 -0400

    refactor: change from Pyo3 to Tauri with Yew

--- rsbuild/src/main.rs
+++ rsbuild/src/main.rs
@@ -4,8 +4,9 @@ use std::{fs::File, io::BufReader};
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #[cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+//
 #[tauri::command]
-fn greet(name: &str) {
+fn run() {
     // print the path
     let file = File::open("../test/input.srt");
     let reader = BufReader::new(file.unwrap());
@@ -17,7 +18,7 @@ fn greet(name: &str) {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![run])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
